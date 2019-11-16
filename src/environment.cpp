c4847210b5fafe4a828bc771729e227048371408
Martin Azkarate <martin.azkarate@gmail.com>
2019-11-16 13:24:53 +0100

    Clustering function usign pcl EuclideanClusterExtraction

--- src/environment.cpp
+++ src/environment.cpp
@@ -54,8 +54,21 @@ void simpleHighway(pcl::visualization::PCLVisualizer::Ptr& viewer)
     // TODO:: Create point processor
     ProcessPointClouds<pcl::PointXYZ>* pointProcessor = new ProcessPointClouds<pcl::PointXYZ>();
     std::pair<pcl::PointCloud<pcl::PointXYZ>::Ptr, pcl::PointCloud<pcl::PointXYZ>::Ptr> segmentCloud = pointProcessor->SegmentPlane(cloud,100,0.2);
-    renderPointCloud(viewer,segmentCloud.first,"obstacleCloud",Color(1,0,0));
-    renderPointCloud(viewer,segmentCloud.second,"roadCloud",Color(0,1,0));
+    //renderPointCloud(viewer,segmentCloud.first,"obstacleCloud",Color(1,0,0));
+    //renderPointCloud(viewer,segmentCloud.second,"roadCloud",Color(0,1,0));
+
+    std::vector<pcl::PointCloud<pcl::PointXYZ>::Ptr> cloudClusters = pointProcessor->Clustering(segmentCloud.first, 1.0, 3, 30);
+
+    int clusterId = 0;
+    std::vector<Color> colors = {Color(1,0,0), Color(1,1,0), Color(0,0,1)};
+
+    for(pcl::PointCloud<pcl::PointXYZ>::Ptr cluster : cloudClusters)
+    {
+      std::cout << "cluster size ";
+      pointProcessor->numPoints(cluster);
+      renderPointCloud(viewer,cluster,"obstCloud"+std::to_string(clusterId),colors[clusterId%colors.size()]);
+      ++clusterId;
+    }
 }
 
 
