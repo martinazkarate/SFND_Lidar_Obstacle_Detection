30ef49ad34a1069f94bf0574f80f8a10c579ac12
Martin Azkarate <martin.azkarate@gmail.com>
2019-11-18 14:52:35 +0100

    search point recursive function in kd tree

--- src/quiz/cluster/kdtree.h
+++ src/quiz/cluster/kdtree.h
@@ -50,10 +50,28 @@ struct KdTree
 		insertRecursive(&root,0,point,id);		
 	}
 
+	void searchRecursive(Node* node, uint depth, std::vector<float> target, float distanceTol, std::vector<int>& ids)
+	{
+		if (node != NULL)
+		{
+			if (sqrt((target[0]-node->point[0])*(target[0]-node->point[0])+(target[1]-node->point[1])*(target[1]-node->point[1]))<=distanceTol)
+			{
+				ids.push_back(node->id);
+				std::cout<<"Found nearby node " << node->id << " at {" << node->point[0] << "," << node->point[1] << "}" << std::endl;
+			}				
+
+			if ((target[depth%2]-distanceTol)<node->point[depth%2])
+				searchRecursive(node->left,depth+1,target,distanceTol,ids);
+			if ((target[depth%2]+distanceTol)>node->point[depth%2])
+				searchRecursive(node->right,depth+1,target,distanceTol,ids);
+		}
+	}
+
 	// return a list of point ids in the tree that are within distance of target
 	std::vector<int> search(std::vector<float> target, float distanceTol)
 	{
 		std::vector<int> ids;
+		searchRecursive(root,0,target,distanceTol,ids);
 		return ids;
 	}
 	
